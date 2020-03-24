2ece820293941729dd9c31afb7a8f36d58e73dd0
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2020-03-24 20:40:48 +0100

    test

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/") Response
+     */
+    public function homepage() {
+        return new Response('OMG! My first page already!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug) {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug
+        ));
+
+    }
+}
