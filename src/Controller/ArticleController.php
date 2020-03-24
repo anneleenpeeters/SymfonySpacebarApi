94dbd34b49ed4a1b32d6213ae4e6a0b1ea5ac10a
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2020-03-24 23:14:45 +0100

    jow

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -28,7 +28,6 @@ class ArticleController extends AbstractController
             'Jow swag dit is anneleen',
             'comments over here',
         ];
-        dump( $slug, $this );
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'comments' => $comments,
